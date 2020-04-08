bb5e5a79bb1a56de18b4ab0f23f08e56c54c51b2
JulesMunschy <j.munschy@gmail.com>
2020-04-08 16:49:09 +0200

    add router in app

--- src/frontend/components/App/index.js
+++ src/frontend/components/App/index.js
@@ -1,21 +1,46 @@
 // == Import npm
 import React from 'react';
+import classNames from 'classnames';
+import { Route, Switch, useLocation } from 'react-router-dom';
 
 import Header from 'src/frontend/components/Header';
 import Footer from 'src/frontend/components/Footer';
 
-import Details from 'src/frontend/components/Offer/Details';
+import Home from 'src/frontend/components/Home';
+import About from 'src/frontend/components/About';
+import Offer from 'src/frontend/components/Offer';
 
 // == Composant
-const App = () => (
-  <div className="app">
-    <Header />
-    <main>
-      <Home />
-    </main>
-    <Footer />
-  </div>
-);
+const App = () => {
+
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const isHome = currentPath === '/';
+  const appClass = classNames('app', { 'app--light': isHome });
+
+  return (
+    <div className={appClass}>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/a-propos">
+            <About />
+          </Route>
+          <Route exact path="/recherche/jeux">
+            <Offer />
+          </Route>
+          <Route>
+            <p>erreur 404</p>
+          </Route>
+        </Switch>
+      </main>
+      <Footer isHome={isHome} />
+    </div>
+  );
+};
 
 // == Export
 export default App;
--- src/frontend/components/Home/index.js
+++ src/frontend/components/Home/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-
-
 import './home.scss';
 
 const Home = () => (
