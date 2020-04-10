33874c6c1fb3d01871a7949bfa25423c02667285
Hanriot Alexandre <hanriot.alexandre@gmail.com>
2020-04-10 09:08:40 +0200

    quick fixed

--- src/frontend/components/App/index.js
+++ src/frontend/components/App/index.js
@@ -59,10 +59,9 @@ const App = () => {
 
           <Route exact path="/compte">
             <Account />
-
+          </Route>
           <Route exact path="/compte/reservations">
             <Reservations />
-
           </Route>
           <Route>
             <p>erreur 404</p>
