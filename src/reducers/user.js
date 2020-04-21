dadb4cdeab71b1c4913062fd8718af730d6d1e2a
JulesMunschy <57390384+JulesMunschy@users.noreply.github.com>
2020-04-21 22:08:11 +0200

    Update user.js

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -9,7 +9,6 @@ import {
   SAVE_USERS,
 } from 'src/actions/user';
 
-/*
 const initialState = {
   allUsers: [],
   email: '',
@@ -42,43 +41,6 @@ const initialState = {
   loginError: '',
   requestIsLoad: false,
 };
-*/
-
-// --------------------------- DELETE TEST
-const initialState = {
-  allUsers: [],
-  email: '',
-  password: '',
-  isLogged: true,
-  userData: {
-    user: {
-      id: 29,
-      role: '0',
-      status: '0',
-      email: 'test28@email.com',
-      password: '$2b$08$7SiX/XoGdGHONDgcwdfMXeX3fb6zn5fNQp4n.dw94LgS64LIJKcf6',
-      username: 'julesobiwanyodamanon',
-      firstname: 'Pepito',
-      lastname: 'Escobar',
-      display_name: false,
-      phone: null,
-      address: null,
-      postal_code: '75011',
-      city: 'Paris',
-      gdpr_accepted_at: null,
-      picture: null,
-      createdAt: '2020-04-21T07:03:17.549Z',
-      updatedAt: '2020-04-21T07:03:17.549Z',
-      old_password: '',
-      new_password: '',
-      confirm_new_password: '',
-    },
-    xsrfToken: '',
-  },
-  loginError: '',
-  requestIsLoad: false,
-};
-
 
 const userReducer = (state = initialState, action = {}) => {
   switch (action.type) {
